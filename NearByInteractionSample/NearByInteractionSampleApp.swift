306290f5741bdb402446b320179fb6fe825e7662
aizen0405 <aizent0405@gmail.com>
2024-07-15 22:46:37 +0900

    Initial Commit

---
+++ NearByInteractionSample/NearByInteractionSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearByInteractionSampleApp.swift
+//  NearByInteractionSample
+//
+//  Created by takanori on 2024/07/15.
+//
+
+import SwiftUI
+
+@main
+struct NearByInteractionSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
